dc028e94666cdb727e67b6d5e5026c5f53b0e3fc
Stanislav Melekhov <devilportez@icloud.com>
2020-08-06 19:26:15 +0500

    WIP

--- src/main.js
+++ src/main.js
@@ -17,6 +17,42 @@ const tripMain = pageBody.querySelector(`.trip-main`);
 const tripControls = tripMain.querySelector(`.trip-controls`);
 const tripEvents = pageBody.querySelector(`.trip-events`);
 
+const pointTypes = [
+  `Taxi`,
+  `Bus`,
+  `Train`,
+  `Ship`,
+  `Transport`,
+  `Drive`,
+  `Flight`,
+  `Check`,
+  `Sightseeing`,
+  `Restaurant`,
+];
+
+const cities = [
+  `New York`,
+  `Seattle`,
+  `Toronto`,
+  `Vancuver`,
+];
+
+const getRandomInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const generateMockData = () => {
+  return {
+    pointType: pointTypes[getRandomInteger(0, pointTypes.length - 1)],
+    destination: cities[getRandomInteger(0, cities.length - 1)],
+  };
+};
+
+console.log(generateMockData());
+
 render(tripInfo, tripMain, `afterbegin`);
 render(tabs, tripControls, `beforeend`);
 render(filters, tripControls, `beforeend`);
