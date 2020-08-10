9c790ae00382cddfa91ba75ea2d151be19496e6f
Stanislav Melekhov <devilportez@icloud.com>
2020-08-10 23:57:07 +0500

    WIP 2

--- src/main.js
+++ src/main.js
@@ -37,6 +37,8 @@ const cities = [
   `Vancuver`,
 ];
 
+const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`
+
 const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -48,6 +50,10 @@ const generateMockData = () => {
   return {
     pointType: pointTypes[getRandomInteger(0, pointTypes.length - 1)],
     destination: cities[getRandomInteger(0, cities.length - 1)],
+    destinationInfo: {
+      description,
+      photo: `http://picsum.photos/248/152?r=${Math.random()}`
+    },
   };
 };
 
